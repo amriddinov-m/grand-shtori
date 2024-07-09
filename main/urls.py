c6ae0245162d80f11e24b5772dd33af08e5d3d92
amriddinov-m <75161328+amriddinov-m@users.noreply.github.com>
2024-07-09 10:08:56 +0500

    ssl

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from main.views import HomeView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('.well-known/pki-validation/83579306C8035DE45221B281A5E5D2B7.txt', TemplateView.as_view(
+        template_name="83579306C8035DE45221B281A5E5D2B7.txt", content_type="text/plain")),
 ]
